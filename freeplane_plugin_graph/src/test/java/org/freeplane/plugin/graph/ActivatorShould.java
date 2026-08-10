110841c3f2f12aa4d068571fa67f644e517ec079
hyperdreamer <wenhepku@gmail.com>
2026-08-11 00:49:51 +0800

    2026-08-10-graph-workspace: Strengthen graph plugin lifecycle test

--- freeplane_plugin_graph/src/test/java/org/freeplane/plugin/graph/ActivatorShould.java
+++ freeplane_plugin_graph/src/test/java/org/freeplane/plugin/graph/ActivatorShould.java
@@ -1,6 +1,5 @@
 package org.freeplane.plugin.graph;
 
-import java.lang.reflect.Field;
 import java.util.Dictionary;
 
 import org.freeplane.features.mode.mindmapmode.MModeController;
@@ -8,14 +7,18 @@ import org.freeplane.main.osgi.IModeControllerExtensionProvider;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
+import org.mockito.MockedConstruction;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class ActivatorShould {
 
@@ -41,22 +44,22 @@ public class ActivatorShould {
 
     @Test
     public void closeExtensionBeforeUnregisteringIt() throws Exception {
-        GraphModeExtension extension = mock(GraphModeExtension.class);
+        BundleContext context = mock(BundleContext.class);
         ServiceRegistration<?> registration = mock(ServiceRegistration.class);
+        when(context.registerService(
+            eq(IModeControllerExtensionProvider.class.getName()),
+            any(IModeControllerExtensionProvider.class),
+            any(Dictionary.class))).thenReturn(registration);
         Activator activator = new Activator();
-        setField(activator, "extension", extension);
-        setField(activator, "extensionRegistration", registration);
 
-        activator.stop(null);
+        try (MockedConstruction<GraphModeExtension> constructions = mockConstruction(GraphModeExtension.class)) {
+            activator.start(context);
+            activator.stop(context);
 
-        InOrder order = inOrder(extension, registration);
-        order.verify(extension).close();
-        order.verify(registration).unregister();
-    }
-
-    private static void setField(Object target, String fieldName, Object value) throws Exception {
-        Field field = Activator.class.getDeclaredField(fieldName);
-        field.setAccessible(true);
-        field.set(target, value);
+            GraphModeExtension extension = constructions.constructed().get(0);
+            InOrder order = inOrder(extension, registration);
+            order.verify(extension).close();
+            order.verify(registration).unregister();
+        }
     }
 }
