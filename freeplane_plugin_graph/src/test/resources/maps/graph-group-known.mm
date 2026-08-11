b66a8a3ac09222cd12f34e469fb6d39c3eccbcf4
hyperdreamer <wenhepku@gmail.com>
2026-08-11 11:26:19 +0800

    2026-08-10-graph-workspace: Strengthen Graph Group regressions

--- freeplane_plugin_graph/src/test/resources/maps/graph-group-known.mm
+++ freeplane_plugin_graph/src/test/resources/maps/graph-group-known.mm
@@ -1,6 +1,17 @@
-<map version="freeplane 1.12.0">
-  <node TEXT="root" ID="ID_ROOT">
-    <node TEXT="marked leaf" ID="ID_MARKED"><graph_group version="1"/></node>
-    <node TEXT="plain leaf" ID="ID_PLAIN"/>
+<map version="freeplane 1.12.0" unrelated_map_attribute="preserve-map">
+  <map_unknown_one order="1" map_extra="alpha"/>
+  <map_unknown_two order="2" map_extra="beta"/>
+  <node TEXT="fixture root" ID="ID_ROOT" root_attribute="preserve-root">
+    <root_unknown_one order="1" root_extra="alpha"/>
+    <root_unknown_two order="2" root_extra="beta"/>
+    <node TEXT="marked leaf" ID="ID_MARKED" marked_attribute="preserve-marked">
+      <known_unknown_one order="1" marker_extra="before"/>
+      <graph_group version="1"/>
+      <known_unknown_two order="2" marker_extra="after"/>
+    </node>
+    <node TEXT="plain leaf" ID="ID_PLAIN" plain_attribute="preserve-plain">
+      <plain_unknown_one order="1" plain_extra="one"/>
+      <plain_unknown_two order="2" plain_extra="two"/>
+    </node>
   </node>
 </map>
--- freeplane_plugin_graph/src/test/resources/maps/graph-group-unknown-version.mm
+++ freeplane_plugin_graph/src/test/resources/maps/graph-group-unknown-version.mm
@@ -1,9 +1,20 @@
-<map version="freeplane 1.12.0">
-  <node TEXT="root" ID="ID_ROOT">
-    <node TEXT="unsupported" ID="ID_UNSUPPORTED">
-      <unknown_before order="1"/>
-      <graph_group version="99" extra="preserve"><nested flag="yes">payload</nested></graph_group>
-      <unknown_after order="2"/>
+<map version="freeplane 1.12.0" unrelated_map_attribute="preserve-map">
+  <map_unknown_one order="1" map_extra="alpha"/>
+  <map_unknown_two order="2" map_extra="beta"/>
+  <node TEXT="fixture root" ID="ID_ROOT" root_attribute="preserve-root">
+    <root_unknown_one order="1" root_extra="alpha"/>
+    <root_unknown_two order="2" root_extra="beta"/>
+    <node TEXT="unsupported leaf" ID="ID_UNSUPPORTED" unsupported_attribute="preserve-unsupported">
+      <unknown_before order="1" unknown_extra="before"/>
+      <graph_group version="99" extra="preserve" raw="keep">
+        <nested flag="yes">payload</nested>
+        <nested_second flag="no">tail</nested_second>
+      </graph_group>
+      <unknown_after order="2" unknown_extra="after"/>
+    </node>
+    <node TEXT="plain leaf" ID="ID_PLAIN" plain_attribute="preserve-plain">
+      <plain_unknown_one order="1" plain_extra="one"/>
+      <plain_unknown_two order="2" plain_extra="two"/>
     </node>
   </node>
 </map>
