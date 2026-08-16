183e46bf96418e4f7209f6669816f0fe20af1a8f
hyperdreamer <wenhepku@gmail.com>
2026-08-16 09:14:19 +0800

    2026-08-10-graph-workspace: Publish live immutable graph state

---
+++ freeplane_plugin_graph/src/main/java/org/freeplane/plugin/graph/control/GraphProjectionListener.java
@@ -0,0 +1,7 @@
+package org.freeplane.plugin.graph.control;
+
+import org.freeplane.plugin.graph.projection.GraphProjection;
+
+public interface GraphProjectionListener {
+    void onGraphProjection(GraphProjection projection);
+}
